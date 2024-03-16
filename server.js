79433111932bd86900143c433526ce5f831c047a
dellsony7 <eng.dellsony@gmail.com>
2024-03-16 11:50:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+ const express = require('express')
+
+ const app = express()
+
+ // PORT
+ const PORT = 5000
+
+// Start the server
+app.listen(PORT, console.log(`Server is up and running on port ${PORT}`))
